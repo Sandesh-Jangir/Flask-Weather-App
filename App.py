282079c0927c814176f260583a58915a6e97fbc5
Sandesh Jangir <69182611+Sandesh-Jangir@users.noreply.github.com>
2022-06-13 16:40:43 +0530

    Increased code's readability

--- App.py
+++ App.py
@@ -3,53 +3,60 @@ import json
 from flask import Flask, render_template, redirect, request
 
 # Variables
-Humidity = ""
-Temperature= ""
-TimeZone = ""
-Clouds = ""
+humidity = ""
+temperature= ""
+time_zone = ""
+clouds = ""
 
 app = Flask(__name__)
 
 @app.route("/getweather", methods=['GET', 'POST'])
 def getWeather():
     if request.method == 'POST':
-        global Humidity
-        global Temperature
-        global TimeZone
-        global Clouds
-
+        # Importing Global Variables
+        global humidity
+        global temperature
+        global time_zone
+        global clouds
+        
+        # Fetching latitude and longitude from the User
         latitude = int(request.form['lat'])
         longitude = int(request.form['lon'])
-
-        apiurl = f"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}.44&lon={longitude}.04&exclude=hourly,daily&appid=98f3a2dc3ba15d13d4a2d1b821f319eb"
         
-        data = requests.get(apiurl).text
-        JsonData = json.loads(data)
+        # Fetching data from the api
+        api_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}.44&lon={longitude}.04&exclude=hourly,daily&appid=98f3a2dc3ba15d13d4a2d1b821f319eb"
+        
+        # Transforming fetched data into JSONified data
+        data = requests.get(api_url).text
+        jsonified_data = json.loads(data)
+        
+        # Fetching weather data of current time
+        current_weather_data = jsonified_data['current']
 
-        CurrentData = JsonData['current']
-
-        Humidity = CurrentData['humidity']
-        Temperature = CurrentData['temp']
-        TimeZone = JsonData['timezone']
-        Clouds = CurrentData['clouds']
+        humidity = current_weather_data['humidity']
+        temperature = current_weather_data['temp']
+        time_zone = jsonified_data['timezone']
+        clouds = current_weather_data['clouds']
 
     
     return redirect('/')
 
 @app.route("/")
-def Home():
-    global Humidity
-    global Temperature
-    global TimeZone
-    global Clouds
-    CelTemp = ""
+def Main():
+    # Importing Global Variables
+    global humidity
+    global temperature
+    global time_zone
+    global clouds
+    temperature_in_celcius = ""
 
-    if Temperature == "":
+    if temperature == "":
         pass
     else:
-        CelTemp = round(int(Temperature) - 273.15, 0)
-
-    return render_template("Main.html", props=[CelTemp, Humidity, Clouds, TimeZone])
+        # Converting Temperature to Celcius
+        temperature_in_celcius = round(int(temperature) - 273.15, 0)
+    # Rendering HTML file and props
+    return render_template("Main.html", props=[temperature_in_celcius, humidity, clouds, time_zone])
 
 if __name__ == "__main__":
     app.run(debug=True)
